903632ae6af562c7f634f5d91f8cd6ea4d38582f
root <root@d2rh8ansb.stg.sita.aero>
2022-07-14 06:39:36 +0100

    Added code change to display new message also updated displayimage tag script to handel error

--- task1/displayImageTag.sh
+++ task1/displayImageTag.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Usage: ./displayImageTag.sh <remote-registry-prefix> <app-name>
+# Usage: ./displayImageTag.sh <registry-prefix> <app-name>
 
 prefix=$1
 name=$2
 
-if [ "$prefix" == "" ] || [ "$name" == ""]  ; then
+if [ "$prefix" == "" ] || [ "$name" == "" ]  ; then
         echo "Usage:
-  $0 <path to docker file> <registry-prefix> <app-name>
+  $0 <registry-prefix> <app-name>
 where
     <registry-prefix> is the prefix  registry, e.g '10.101.210.100:5000/'
     <app-name> is the name of container image
@@ -16,7 +16,7 @@ where
 fi
 
 
-latestTag=`curl --insecure --silent http://$registry-prefix/v2/$app-name/tags/list  | sed -e 's/"//g' | sed -e 's/{name://g' | sed -e 's/]}//g' | sed -e 's/,tags://g' | tr -s '[' ' ' | awk '{ split($2,a,","); for (i in a) print $1, a[i]; }' | sed -e 's/ /:/g' | tail -1`
+latestTag=`curl --insecure --silent http://$prefix/v2/$name/tags/list  | sed -e 's/"//g' | sed -e 's/{name://g' | sed -e 's/]}//g' | sed -e 's/,tags://g' | tr -s '[' ' ' | awk '{ split($2,a,","); for (i in a) print $1, a[i]; }' | sed -e 's/ /  ->  /g' | tail -1`
 
 echo "Latest tag details is :: -> " $latestTag
 
